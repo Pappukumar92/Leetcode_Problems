c8368f9d6d2a2b5be1f99ecb540e0503272a9a23
Pappu Kumar <161245494+Pappukumar92@users.noreply.github.com>
2026-02-25 23:30:12 +0530

    Time: 0 ms (100%), Space: 9.9 MB (53.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -8,7 +8,7 @@ public:
         while (start < end) {
             while (start < end && !isalnum(s[start]))
                 start++;
-
+//isalnum is a cpp function to check alphanumeric or not (A-Z,a=z,0-9)
             while (start < end && !isalnum(s[end]))
                 end--;
             if (tolower(s[start]) != tolower(s[end]))
