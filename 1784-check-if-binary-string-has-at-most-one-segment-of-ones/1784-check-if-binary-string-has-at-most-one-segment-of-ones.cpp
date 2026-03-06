4d3cf49efea8b06d63bc611d1b8f61e0fd70834a
Pappu Kumar <161245494+Pappukumar92@users.noreply.github.com>
2026-03-07 00:46:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
-    bool checkOnesSegment(string s) {
-        return s.find("01")==string::npos;
+    bool checkOnesSegment(string s) 
+    {
+        for(int i =1;i<s.size();i++)
+        {
+            if(s[i]=='1' && s[i-1]=='0') return false;
+        }
+        return true;
     }
 };
