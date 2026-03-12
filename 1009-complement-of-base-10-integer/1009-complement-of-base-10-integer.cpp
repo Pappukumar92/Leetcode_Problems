8b04adc4c8dc703ddc8215c56e18c44849d7f7cb
Pappu Kumar <161245494+Pappukumar92@users.noreply.github.com>
2026-03-12 15:35:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
+    int solve(int n, int pos){
+        if(n == 0) return 0;
+
+        int bit = n % 2;
+        int flipped = 1 - bit;
+
+        return flipped * pos + solve(n / 2, pos * 2);
+    }
     int bitwiseComplement(int n) {
         if(n == 0) return 1;
-        string b = "";
-        while(n > 0){
-            b = char((n % 2) + '0') + b;
-            n /= 2;
-        }
-        for(int i = 0; i < b.size(); i++){
-            if(b[i] == '0') b[i] = '1';
-            else b[i] = '0';
-        }
-        int ans = 0;
-        for(char c : b){
-            ans = ans * 2 + (c - '0');
-        }
-        return ans;
+        return solve(n, 1);
     }
 };
