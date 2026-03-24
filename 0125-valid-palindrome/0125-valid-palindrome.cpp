98b1a2659813c50f8f9c7c3c45b60ae893c80e7a
Pappu Kumar <161245494+Pappukumar92@users.noreply.github.com>
2026-03-24 18:21:45 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.48%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        int start = 0;
-        int end = s.length() - 1;
-
-        while (start < end) {
-            while (start < end && !isalnum(s[start]))
-                start++;
-//isalnum is a cpp function to check alphanumeric or not (A-Z,a=z,0-9)
-            while (start < end && !isalnum(s[end]))
-                end--;
-            if (tolower(s[start]) != tolower(s[end]))
-                return false;
-            start++;
-            end--;
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right)
+        {
+            if(!isalnum(s[left]))
+            {
+                left++;
+                continue;
+            }
+            if(!isalnum(s[right]))
+            {
+                right--;
+                continue;
+            }
+            if(tolower(s[left]) != tolower(s[right]))
+            return false;
+            left++;
+            right--;
         }
         return true;
     }
