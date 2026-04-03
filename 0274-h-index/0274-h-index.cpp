3837ba0ce5493137fd0e57512ed608703f4b4912
Pappu Kumar <161245494+Pappukumar92@users.noreply.github.com>
2026-04-03 23:06:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-     sort(citations.begin(),citations.end(),greater<int>());
-     int h = 0;
-     for(int i =0;i<citations.size();i++)
-     {
-        if(citations[i]>= i+1)
-        h = i+1;
-        else
-        break;
-     }   
-     return h;
+        int n = citations.size();
+        vector<int>count(n+1,0);
+        for(int c : citations)
+        {
+            if(c>=n)
+            count[n]++;
+            else
+            count[c]++;
+        }
+        int papers = 0;
+        for(int i =n;i>=0;i--)
+        {
+            papers += count[i];
+            if(papers >= i)
+            return i;
+        }
+        return 0;
     }
 };
