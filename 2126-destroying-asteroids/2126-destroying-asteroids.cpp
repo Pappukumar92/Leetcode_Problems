067904f22f68458d3631668e54e3e7678939f1a3
Pappu Kumar <161245494+Pappukumar92@users.noreply.github.com>
2026-05-31 22:06:09 +0530

    Time: 43 ms (10.07%), Space: 106.7 MB (48.03%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(), asteroids.end());
-        long long sum = mass;
-        for(int i = 0; i<asteroids.size(); i++){
-            if(asteroids[i]>sum)
-            return false;
-            sum += asteroids[i];
+        
+        long long cur = mass;
+        for (int a : asteroids) {
+            if (cur < a) return false;
+            cur += a;
         }
+        
         return true;
     }
 };
